ec45b6228ac62e2ef911ba946b1bac19b14c481f
Tugaytalha <mukoicen@gmail.com>
2024-10-17 14:17:59 +0300

    tradeoff exploration / exploitation logic changed, some functions implemented
    Epsilon logic changed to exponantial decrement instead of linear for more smooth decrease
    remember
    train_short_memory
    train_long_memory implemented

--- agent.py
+++ agent.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import random
 import numpy as np
@@ -8,6 +10,7 @@ MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
+
 class Agent:
 
     def __init__(self):
@@ -15,23 +18,49 @@ class Agent:
         self.epsilon = 0  # randomness
         self.gamma = 0  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
-        # TODO: modei, trainer
-
+        self.model = None  # TODO: model
+        self.trainer = None  # TODO: trainer
 
     def get_state(self, game):
         pass
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        # unpack memory
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        # random moves: tradeoff exploration / exploitation
+        # formula for epsilon: {\left(20-x^{0.5}\right)^{1.9}}
+        try:
+            self.epsilon = (20 - math.sqrt(self.n_games)) ** 1.9
+        except:
+            self.epsilon = 0
+
+        # Create a list of zeros for the 138 possible moves (4 directions + 64 * 2(horizontal, vertical) walls)
+        final_move = [0] * 138
+        if random.randint(0, 400) < self.epsilon:
+            move = random.randint(0, 5)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
 
 def train():
     plot_scores = []
@@ -75,6 +104,7 @@ def train():
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             # plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
     train()
